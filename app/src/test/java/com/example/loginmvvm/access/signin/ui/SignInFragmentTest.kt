e1bffb0c4ae90a5c353564a19f0cc80e00588a4a
marcio.borges <ctl_mborges@uolinc.com>
2023-07-19 21:57:27 -0300

    Config gradle test signin

--- app/src/test/java/com/example/loginmvvm/access/signin/ui/SignInFragmentTest.kt
+++ app/src/test/java/com/example/loginmvvm/access/signin/ui/SignInFragmentTest.kt
@@ -1,9 +1,15 @@
 package com.example.loginmvvm.access.signin.ui
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import com.example.loginmvvm.R
+import io.mockk.clearAllMocks
+import io.mockk.unmockkAll
+import org.junit.AfterClass
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
@@ -11,10 +17,15 @@ import org.robolectric.annotation.Config
 @Config(minSdk = 29)
 class SignInFragmentTest {
 
+    @Before
+    fun setup() {
+        clearAllMocks()
+    }
+
     @Test
     fun `given open screen then show components`() {
-        val scenario = launchFragmentInContainer<SignInFragment>(themeResId = R.style.AppTheme_NoActionBar)
-        scenario.onFragment { fragment ->
+        val cs = FragmentScenario.launchInContainer(SignInFragment::class.java, themeResId = R.style.AppTheme_NoActionBar)
+        cs.onFragment { fragment ->
             SignInFragmentRobot().start(fragment) {
                 it.checkIsVisibleTitle()
                 it.checkIsVisibleSubTitle()
@@ -23,6 +34,26 @@ class SignInFragmentTest {
                 it.checkIsVisibleInputPassword()
                 it.checkIsVisibleButton()
             }
+        }
+//        val scenario = launchFragmentInContainer<SignInFragment>(themeResId = R.style.AppTheme_NoActionBar)
+//        scenario.onFragment { fragment ->
+//            SignInFragmentRobot().start(fragment) {
+//                it.checkIsVisibleTitle()
+//                it.checkIsVisibleSubTitle()
+//                it.checkIsVisibleDescription()
+//                it.checkIsVisibleInputEmail()
+//                it.checkIsVisibleInputPassword()
+//                it.checkIsVisibleButton()
+//            }
+//        }
+    }
+
+    companion object {
+        @JvmStatic
+        @AfterClass
+        fun unloadInjectionAndMocks() {
+            stopKoin()
+            unmockkAll()
         }
     }
 }
