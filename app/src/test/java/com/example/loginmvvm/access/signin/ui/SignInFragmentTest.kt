691d6446454fc14c3306d840bac0ca17f4b29ad9
marcio.borges <ctl_mborges@uolinc.com>
2023-07-21 12:02:55 -0300

    [EZ1-T18] - Test fragment

--- app/src/test/java/com/example/loginmvvm/access/signin/ui/SignInFragmentTest.kt
+++ app/src/test/java/com/example/loginmvvm/access/signin/ui/SignInFragmentTest.kt
@@ -1,13 +1,18 @@
 package com.example.loginmvvm.access.signin.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.loginmvvm.R
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SignInFragmentTest {
+
+    @get:Rule
+    var testSchedulerRule = InstantTaskExecutorRule()
 
     @Test
     fun `given open screen then show components`() {
