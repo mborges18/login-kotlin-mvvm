8882364a9afb6a3015bef16f13bb9afcb27f6b83
marcio.borges <ctl_mborges@uolinc.com>
2023-07-23 13:41:24 -0300

    [EZ1-T18] - Test Fragment

--- app/src/test/java/com/example/loginmvvm/access/signin/ui/SignInFragmentTest.kt
+++ app/src/test/java/com/example/loginmvvm/access/signin/ui/SignInFragmentTest.kt
@@ -3,12 +3,14 @@ package com.example.loginmvvm.access.signin.ui
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.loginmvvm.R
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SignInFragmentTest {
 
+    @Ignore("Test is ignored as a demonstration")
     @Test
     fun `given open screen then show components`() {
         val scenario = launchFragmentInContainer<SignInFragment>(themeResId = R.style.AppTheme_NoActionBar)
